6942079a6487de94115248d2381d9c9aa60d8af0
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2024-03-31 17:04:18 +0800

    init

---
+++ src/main/java/lee/ramnants/ramnants_qt_bot/RamnantsQtBotApplication.java
@@ -0,0 +1,13 @@
+package lee.ramnants.ramnants_qt_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamnantsQtBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamnantsQtBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lee/ramnants/ramnants_qt_bot/RamnantsQtBotApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.ramnants.ramnants_qt_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamnantsQtBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
